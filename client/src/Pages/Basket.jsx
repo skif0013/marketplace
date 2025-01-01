67acc051f47798ca0a54451095a0b44250af84b8
Ridik <kobrinchuk3310@gmail.com>
2025-01-01 14:50:00 +0200

    Добавил код Руслана

--- client/src/Pages/Basket.jsx
+++ client/src/Pages/Basket.jsx
@@ -1,21 +1,45 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
-import { BasketEmpty, BasketMain } from "../components/Basket/Basket";
+import { BasketEmpty, BasketMain } from '../components/Basket/Basket.jsx';
+import React, { useState } from "react";
 
 export default function Basket() {
-   let basket = ['d'];
+   const [basket, setBasket] = useState([
+      { id: "3490387208", title: "Iphone rx 3500", price: 10000, quantity: 20, categories: "Телефоны и аксессуары" },
+      { id: "3208490832084908", title: "Клавиатура на мембране", price: 39398, quantity: 4, categories: "Клавиатуры и Мыши" },
+      { id: "34985439580", title: "Наушники Sony air Bach", price: 560390, quantity: 7, categories: "Наушники и Аксессуары" },
+      { id: "12348239432", title: "Адаптер", price: 308419, quantity: 9, categories: "Сетевое оборудование" },
+      { id: "09868902635", title: "Видео карта RX-3080 it", price: 178456, quantity: 6, categories: "Комплектующие" },
+   ]);
+
+   // Синхронізувати зміни у кошику
+   const handleRemoveItem = (id) => {
+      setBasket(basket.filter((item) => item.id !== id));
+   };
+
+   const handleUpdateQuantity = (id, amount) => {
+      setBasket(
+         basket.map((item) =>
+            item.id === id ? { ...item, quantity: Math.max(item.quantity + amount, 1) } : item
+         )
+      );
+   };
+
    return (
-      <div className={`flex flex-col min-h-screen ${basket && basket.length === 0 ? 'bg-orange-100' : ''}`}>
+      <div className={`flex flex-col min-h-screen ${basket.length === 0 ? "bg-orange-100" : ""}`}>
          <Header />
          <main className="flex-grow mb-52 mt-20">
-            {basket && basket.length === 0 ? (
+            {basket.length === 0 ? (
                <BasketEmpty />
             ) : (
-               <BasketMain />
+               <BasketMain
+                  basket={basket}
+                  onRemove={handleRemoveItem}
+                  onUpdateQuantity={handleUpdateQuantity}
+               />
             )}
          </main>
          <Footer />
       </div>
-
-   )
+   );
 }
