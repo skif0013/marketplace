eb45029c82f7cdc00298c9730e5407d6794c975b
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-01-03 16:30:35 +0200

    Закончил стилизацию формы

--- client/src/Pages/Sell/Sell.jsx
+++ client/src/Pages/Sell/Sell.jsx
@@ -2,18 +2,17 @@ import React, {useState} from 'react';
 import Header from "../../components/Header/Header.jsx";
 import Step1 from "../../components/Steps/Step1.jsx";
 import Step2 from "../../components/Steps/Step2.jsx";
+import useRersistedState from "../../hooks/useRersistedState.js";
 
 function Sell() {
-    const [step, setStep] = useState(1);
+    const [step, setStep] = useRersistedState('currentStep', 1);
     const handleClickNext = () => {
         setStep((prevStep) => (prevStep < 2 ? prevStep + 1 : prevStep))
-        console.log(step)
 
     }
 
     const handlePrevClick = () => {
         setStep((prevStep) => (prevStep > 0 ? prevStep - 1 : prevStep))
-        console.log(step)
     }
     return (
         <div>
--- client/src/components/Steps/Step2.jsx
+++ client/src/components/Steps/Step2.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./steps.module.css";
+import Input from 'react-phone-number-input/input'
 
 function Step2({onBack}) {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [value, setValue] = useState(undefined)
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    const handleChangeName = (e) => {
+        setName(e.target.value)
+    }
+
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handleRegister = () => {
+        if (!emailPattern.test(email)) {
+            alert('Неправильный Email')
+        } else {
+            alert('Welcome)')
+        }
+    }
+
     return (
         <div className={styles.formContainer}>
-            <h1 className = {styles}>Контактная информация</h1>
-            <input placeholder='ФИО' className={styles.formInput}/>
-            <input placeholder='Должность' className={styles.formInput}/>
-            <input placeholder='E-mail' className={styles.formInput}/>
-            <input placeholder='Номер телефона' className={styles.formInput}/>
-            <button onClick={onBack} className={styles.button}>Назад</button>
+            <h1 className={styles.title}>Контактная информация</h1>
+            <input placeholder='Имя' className={styles.formInput} value={name} onChange={handleChangeName}/>
+            <input placeholder='E-mail' className={styles.formInput} value={email} onChange={handleChangeEmail}/>
+            <Input country='UA' placeholder='Номер телефона' className={styles.formInput} value={value}
+                   onChange={setValue}/>
+
+            {(name === '' || email === '' || value === undefined) ? (
+                <label style={{color: 'red'}}>Все поля для ввода обязательны</label>
+            ) : null}
+
+            <div className={styles.buttonContainer}>
+                <button onClick={onBack} className={styles.button}>Назад</button>
+                {(name === '' || email === '' || value === undefined) ? (
+                    <button onClick={onBack} className={styles.registerButton} disabled>Загерестрироваться</button>
+                ) : (
+                    <button onClick={ handleRegister} className={styles.registerButton}>Загерестрироваться</button>
+                )}
+            </div>
+
+
         </div>
     );
 }
